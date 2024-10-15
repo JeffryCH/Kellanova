5438968b02d4a00bc70cab9ca25f3ee000a5d8e3
JeffryCH <jcanales20717@ufide.ac.cr>
2024-10-15 15:28:54 -0600

    se agrega boton para registrar usuario

--- register.js
+++ register.js
@@ -1,6 +1,6 @@
 // Import Firebase SDK modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 
 // Firebase configuration
 const firebaseConfig = {
@@ -9,7 +9,7 @@ const firebaseConfig = {
   projectId: "kellogg-s-d6006",
   storageBucket: "kellogg-s-d6006.appspot.com",
   messagingSenderId: "882591600958",
-  appId: "1:882591600958:web:359b0eb7e54e8592c19c6f",
+  appId: "1:882591600958:web:359b0eb7e54e8592c19c6f"
 };
 
 // Initialize Firebase
@@ -17,28 +17,50 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 document.addEventListener("DOMContentLoaded", () => {
+  const loginButton = document.getElementById("loginButton");
   const registerButton = document.getElementById("registerButton");
   const emailInput = document.getElementById("email");
   const passwordInput = document.getElementById("password");
-  const registerMessage = document.getElementById("registerMessage");
+  const messageElement = document.getElementById("message");
+
+  loginButton.addEventListener("click", () => {
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    if (email === "" || password === "") {
+      messageElement.innerText = "Por favor ingrese un correo electrónico y contraseña válidos.";
+      return;
+    }
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        messageElement.innerText = "Inicio de sesión exitoso.";
+        window.location.href = "https://jeffrych.github.io/Kellanova/dashboard.html";
+      })
+      .catch((error) => {
+        console.error("Error al iniciar sesión:", error);
+        messageElement.innerText = `Error: ${error.message}`;
+      });
+  });
 
   registerButton.addEventListener("click", () => {
     const email = emailInput.value.trim();
     const password = passwordInput.value.trim();
 
     if (email === "" || password === "") {
-      registerMessage.innerText = "Por favor ingrese un correo electrónico y contraseña válidos.";
+      messageElement.innerText = "Por favor ingrese un correo electrónico y contraseña válidos.";
       return;
     }
 
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
-        registerMessage.innerText = "Registro exitoso. Ahora puedes iniciar sesión.";
+        messageElement.innerText = "Registro exitoso. Ahora puedes iniciar sesión.";
       })
       .catch((error) => {
         console.error("Error al registrar el usuario:", error);
-        registerMessage.innerText = `Error: ${error.message}`;
+        messageElement.innerText = `Error: ${error.message}`;
       });
   });
 });
